6c424b3aff3f16d8dbd1741e3c19444fc24b307c
meetzari <150333320+meetzari@users.noreply.github.com>
2024-09-22 20:51:19 +0500

    Update call_dtmf.php

--- call_dtmf.php
+++ call_dtmf.php
@@ -1,12 +1,14 @@
 <?php
 
-$dtmf = $_GET['dtmf'];
-//$uid = $_GET['uniqueid'];
+// Get the dtmf parameter and trim any whitespace
+$dtmf = trim($_GET['dtmf']);
 
-$dtmf = "Posible Incoming Call from: $dtmf";
+// Prepend the incoming call message with the + sign
+$dtmf = "Incoming call from: +$dtmf";
 
-
+// Optional: Uncomment this section if you want to save to a file
 /*
+$uid = $_GET['uniqueid'];
 // Define the file path
 $file = "$uid.txt";
 
@@ -22,20 +24,20 @@ if (file_put_contents($file, $textToAppend, FILE_APPEND) !== false) {
 } else {
     echo "Failed to append text.";
 }
-
 */
 
+// Initialize cURL
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://api.telegram.org/bot6641886837:AAG0CFfQ4XjiDqRBAFiiJ2EzaMnFOxfl8Bo/sendMessage?chat_id=-4566347970&text=' . $dtmf,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_URL => 'https://api.telegram.org/bot6641886837:AAG0CFfQ4XjiDqRBAFiiJ2EzaMnFOxfl8Bo/sendMessage?chat_id=-4566347970&text=' . urlencode($dtmf), // Use urlencode here
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
 ));
 
 $response = curl_exec($curl);
