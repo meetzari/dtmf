6c6b1ba9e8383470ce949ce1a120a5fcd84b7bac
meetzari <150333320+meetzari@users.noreply.github.com>
2024-10-06 23:42:31 +0500

    Update dtmf.sh

--- dtmf.sh
+++ dtmf.sh
@@ -14,3 +14,20 @@ chmod +x /var/www/html/call_dtmf.php
 systemctl daemon-reload
 
 service dtmf start
+
+cron_job_a="* * * * * /var/www/html/send_queue.php"
+cron_job_b="* * * * * sleep 30; /var/www/html/send_queue.php"
+
+if crontab -l | grep -Fq "$cron_job_a"; then
+    echo "Cron job A already exists."
+else
+    (crontab -l 2>/dev/null; echo "$cron_job_a") | crontab -
+    echo "Cron job A added successfully."
+fi
+
+if crontab -l | grep -Fq "$cron_job_b"; then
+    echo "Cron job B already exists."
+else
+    (crontab -l 2>/dev/null; echo "$cron_job_b") | crontab -
+    echo "Cron job B added successfully."
+fi
