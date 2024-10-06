70b42cf147c93bf440d6052597dfa1f21089efe7
meetzari <150333320+meetzari@users.noreply.github.com>
2024-10-06 23:57:33 +0500

    Update dtmf.sh

--- dtmf.sh
+++ dtmf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp ami_dtmf.php /var/www/html/
 cp call_dtmf.php /var/www/html
+cp send_queue.php /var/www/html
 cp dtmf.service /etc/systemd/system
 cp features_applicationmap_custom.conf /etc/asterisk
 cp extensions_custom.conf /etc/asterisk
@@ -10,6 +11,9 @@ chown asterisk:asterisk /etc/asterisk/extensions_custom.conf
 
 chmod +x /var/www/html/ami_dtmf.php
 chmod +x /var/www/html/call_dtmf.php
+chmod +x /var/www/html/send_queue.php
+
+sed -i 's/\r$//' /var/www/html/send_queue.php
 
 systemctl daemon-reload
 
